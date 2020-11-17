b8a5822e267e2a0f6d74aa7b71e07077854945aa
lsongzhi <lsongzhi@163.com>
2020-11-17 22:41:48 +0800

    feat: asm

---
+++ src/asm.rs
@@ -0,0 +1,28 @@
+pub use cortex_a::asm::*;
+use cortex_a::regs::RegisterReadOnly;
+
+/// Returns the current stack pointer.
+#[inline(always)]
+pub fn sp() -> *const u8 {
+    let ptr: usize;
+    unsafe {
+        asm!("mov {0}, sp", out(reg) ptr, options(pure, nomem, nostack));
+    }
+    ptr as *const u8
+}
+
+/// # Safety
+///
+/// Returns the current point counter.
+#[inline(always)]
+pub unsafe fn pc() -> usize {
+    let pc: usize;
+    asm!("adr {}, .", out(reg) pc, options(pure, nomem, nostack));
+    pc
+}
+
+/// CPU id
+#[inline]
+pub fn cpuid() -> usize {
+    (cortex_a::regs::MPIDR_EL1.get() & 3) as usize
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 #![no_std]
+#![feature(asm)]
+#![feature(core_intrinsics)]
 
 pub mod addr;
+pub mod asm;
 
 pub use cortex_a::*;
